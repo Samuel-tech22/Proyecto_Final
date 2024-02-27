f74a97d17e3593ba9f858730c158c56cebcb9811
Natalia Delgado <ilanaadelgad0@gmail.com>
2024-02-27 01:25:36 -0300

    Actualización de la funcionalidad de búsqueda***

--- client/src/app/store/products/page.jsx
+++ client/src/app/store/products/page.jsx
@@ -1,11 +1,12 @@
 import AllProducts from "@/components/AllProducts/AllProducts";
+import Filter from "@/components/Filter/Filter";
 
 export default function ProductsPage(){
 
 
     return (
         <div className="px-28 py-7">
-
+            <Filter/>
         
         <div>
             <AllProducts title= "Productos"></AllProducts>
--- client/src/components/Filter/Filter.jsx
+++ client/src/components/Filter/Filter.jsx
@@ -1,11 +1,13 @@
+"use client"
 import { faFilter } from '@fortawesome/free-solid-svg-icons';
-import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useState } from 'react';
 
 const Filter = () => {
-    const [search, setSearch] = useState("");
     const [marca, setMarca] = useState([]);
     const [talla, setTalla] = useState({});
     const [price, setPrice] = useState({});
+    
 
 
   return (
@@ -13,8 +15,33 @@ const Filter = () => {
 
 
         <button className='border-solid-black'>
-      <FontFaceSet icon={faFilter}/>
+      <FontAwesomeIcon icon={faFilter}/>
+      <span>Filtar</span>
         </button>
+        <div>
+          <h1>Filtrar</h1>
+          <label htmlFor="marca">Marca</label>
+          <select name="marca" id="marca">
+            <option value={marca}>Nike</option>
+            <option value={marca}>Moleca</option>
+            <option value={marca}>Beira Rio</option>
+            <option value={marca}>Adidas</option>
+            <option value={marca}>Puma</option>
+            <option value={marca}>Modare</option>
+          </select>
+          <label htmlFor="talla">Talla</label>
+          <select name="talla" id="talla">
+            <option value={talla}>34</option>
+            <option value={talla}>35</option>
+            <option value={talla}>36</option>
+            <option value={talla}>37</option>
+            <option value={talla}>38</option>
+            <option value={talla}>39</option>
+          </select>
+          <label htmlFor="price">Precio</label>
+          <input type="number" placeholder="Precio mínimo" value={price} />
+          <input type="number" placeholder="Precio máximo" value={price}/>
+        </div>
     </div>
   );
 };
