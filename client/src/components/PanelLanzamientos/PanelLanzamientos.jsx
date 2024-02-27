988234b51919071ccde4938282f336359147df01
Samuel Candia <scandia022@gmail.com>
2024-02-26 22:21:34 -0300

    Se agrego Slider de Lanzamiento

--- client/src/components/PanelLanzamientos/PanelLanzamientos.jsx
+++ client/src/components/PanelLanzamientos/PanelLanzamientos.jsx
@@ -1,45 +1,82 @@
-import { useEffect, useState } from "react";
-import ProductCard from "../ProductCard/ProductCard";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { apiUrl } from "@/config";
-import ImageSlider from "../ImageSlider/ImageSlider";
+import ProductCard from "../ProductCard/ProductCard";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-export default function PanelLanzamientos(){
+const PrevArrow = (props) => (
+  <div
+    className="slick-arrow slick-prev"
+    onClick={props.onClick}
+    style={{ left: "60px", zIndex: 1 }}
+  >
+    Prev
+  </div>
+);
 
-   const [products, setProducts] = useState([]);
-    const [init, setInit] = useState(0);
-   const getLanzamientos = async ()=>{
-    try {
+const NextArrow = (props) => (
+  <div
+    className="slick-arrow slick-next"
+    onClick={props.onClick}
+    style={{ right: "60px", zIndex: 1 }}
+  >
+    Next
+  </div>
+);
+
+export default function PanelLanzamientos() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getLanzamientos = async () => {
+      try {
         const response = await axios.get(`${apiUrl}/products/lanzamientos`);
-        const result = await response.data;
-        setProducts(result);
-
+        setProducts(response.data);
       } catch (error) {
         console.log(error);
       }
-   }
-   
-   useEffect(()=>{
-    if(init === 0){
-        getLanzamientos();
+    };
+    getLanzamientos();
+  }, []);
 
-    }
-    setInit(1);
-   });
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
 
-    return(
-        <div className="w-screen ">
-            <h1 className="text-center text-xl font-bold pb-3">Lanzamientos</h1>
-            
-            <div className="w-full whitespace-nowrap overflow-hidden overflow-x-scroll scroll-smooth focus:scroll-auto">
-                {
-                    products.map((product, index) => {
-                        return (
-                            <ProductCard key={index} product={product}/>
-                        )
-                    })
-                }
-            </div>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h1 className="text-center text-xl font-bold pb-3">Lanzamientos</h1>
+      {products.length > 0 && (
+        <Slider {...settings}>
+          {products.map((product, index) => (
+            <ProductCard key={index} product={product} />
+          ))}
+        </Slider>
+      )}
+    </div>
+  );
+}
