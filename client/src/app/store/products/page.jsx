e67330f0df87ddf1c4085de0491f030d83833f78
Natalia Delgado <ilanaadelgad0@gmail.com>
2024-02-27 19:10:13 -0300

    Actualizar página de productos y agregar componente de filtro

--- client/src/app/store/products/page.jsx
+++ client/src/app/store/products/page.jsx
@@ -1,12 +1,11 @@
 import AllProducts from "@/components/AllProducts/AllProducts";
-import Filter from "@/components/Filter/Filter";
 
 export default function ProductsPage(){
 
 
     return (
         <div className="px-28 py-7">
-            <Filter/>
+
         
         <div>
             <AllProducts title= "Productos"></AllProducts>
